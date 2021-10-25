ded5300f95b87e6f434cd323623b73b758d63ecc
PeterM0ller <s215707@dtu.dk>
2021-10-25 18:34:08 +0200

    Lavet alle felterne og tjekket at de virker

--- src/AdventureDice.java
+++ src/AdventureDice.java
@@ -18,9 +18,45 @@ public class AdventureDice {
         Konto.addSaldo(Felter.Tower());
         System.out.println(Felter.tower());
 
-        System.out.println("Du har slået to");
-        Konto.addSaldo(Felter.Tower());
-        System.out.println(Felter.tower());
+        System.out.println("Du har slået tre");
+        Konto.addSaldo(Felter.Crater());
+        System.out.println(Felter.crater());
+
+        System.out.println("Du har slået fire");
+        Konto.addSaldo(Felter.Palace_gates());
+        System.out.println(Felter.palace_gates());
+
+        System.out.println("Du har slået fem");
+        Konto.addSaldo(Felter.Cold_desert());
+        System.out.println(Felter.cold_desert());
+
+        System.out.println("Du har slået seks");
+        Konto.addSaldo(Felter.Walled_city());
+        System.out.println(Felter.walled_city());
+
+        System.out.println("Du har slået syv");
+        Konto.addSaldo(Felter.Monastery());
+        System.out.println(Felter.monastery());
+
+        System.out.println("Du har slået otte");
+        Konto.addSaldo(Felter.Black_cave());
+        System.out.println(Felter.black_cave());
+
+        System.out.println("Du har slået ni");
+        Konto.addSaldo(Felter.Huts_in_the_mountain());
+        System.out.println(Felter.huts_in_the_mountain());
+
+        System.out.println("Du har slået ti");
+        Konto.addSaldo(Felter.The_werewall());
+        System.out.println(Felter.the_werewall());
+
+        System.out.println("Du har slået elleve");
+        Konto.addSaldo(Felter.The_pit());
+        System.out.println(Felter.the_pit());
+
+        System.out.println("Du har slået tolv");
+        Konto.addSaldo(Felter.Goldmine());
+        System.out.println(Felter.goldmine());
 
 
 
--- src/Felter.java
+++ src/Felter.java
@@ -1,10 +1,112 @@
 public class Felter {
-        public static int Tower(){
-            int x = 250;
-            return x;
-        }
-        public static String tower(){
-            String sætning = "Du har fundet 250 guldmønter, din balance er nu: " + Konto.getSaldo();
-            return sætning;
-        }
+    public static int Tower() {
+        int x = 250;
+        return x;
     }
+
+    public static String tower() {
+        String sætning = "Du går ind i et gammelt tårn og finder 250 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Crater() {
+        int x = -100;
+        return x;
+    }
+
+    public static String crater() {
+        String sætning = "Du falder ned i et crater og mister 100 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Palace_gates() {
+        int x = 100;
+        return x;
+    }
+
+    public static String palace_gates() {
+        String sætning = "Du finder 100 guldmønter udenfor portene til et palads, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Cold_desert() {
+        int x = -20;
+        return x;
+    }
+
+    public static String cold_desert() {
+        String sætning = "Du farer vild i en kold ørken og mister 20 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Walled_city() {
+        int x = 180;
+        return x;
+    }
+
+    public static String walled_city() {
+        String sætning = "Du finder 180 guldmønter uden for en befæstet by, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Monastery() {
+        int x = 0;
+        return x;
+    }
+
+    public static String monastery() {
+        String sætning = "Du går ind i et kloster, men finder ikke nogle guldmønter, din balance er stadig: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Black_cave() {
+        int x = -70;
+        return x;
+    }
+
+    public static String black_cave() {
+        String sætning = "Du farer vild i en mørk grotte og mister 70 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Huts_in_the_mountain() {
+        int x = 60;
+        return x;
+    }
+
+    public static String huts_in_the_mountain() {
+        String sætning = "Du finder nogle hytter i bjergene og finder 60 guldmønter i en af dem, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int The_werewall() {
+        int x = -80;
+        return x;
+    }
+
+    public static String the_werewall() {
+        String sætning = "På din rejse møder du en masse varulve, du skynder dig at løbe væk og taber 80 guldmønter ud af din lomme, men du får en ekstra tur, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int The_pit() {
+        int x = -50;
+        return x;
+    }
+
+    public static String the_pit() {
+        String sætning = "Du falder ned i et dybt hul og er nød til at betale en mand 50 guldmønter for at hjælpe dig op, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+
+    public static int Goldmine() {
+        int x = 650;
+        return x;
+    }
+
+    public static String goldmine() {
+        String sætning = "Du går ind i en guldmine og finder 650 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return sætning;
+    }
+}
+
