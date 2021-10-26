ef50cbe2f46c2a7b4b08ee514803836446b0cf31
PeterM0ller <s215707@dtu.dk>
2021-10-26 15:19:43 +0200

    rettet i felter i forhold til nye spiller klasse

--- src/Felter.java
+++ src/Felter.java
@@ -5,7 +5,7 @@ public class Felter {
     }
 
     public static String tower() {
-        String tekst = "Du går ind i et gammelt tårn og finder 250 guldmønter, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du går ind i et gammelt tårn og finder 250 guldmønter, din balance er nu: ";
         return tekst;
     }
 
@@ -15,7 +15,7 @@ public class Felter {
     }
 
     public static String crater() {
-        String tekst = "Du falder ned i et crater og mister 100 guldmønter, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du falder ned i et crater og mister 100 guldmønter, din balance er nu: ";
         return tekst;
     }
 
@@ -25,7 +25,7 @@ public class Felter {
     }
 
     public static String palace_gates() {
-        String tekst = "Du finder 100 guldmønter udenfor portene til et palads, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du finder 100 guldmønter udenfor portene til et palads, din balance er nu: ";
         return tekst;
     }
 
@@ -35,7 +35,7 @@ public class Felter {
     }
 
     public static String cold_desert() {
-        String tekst = "Du farer vild i en kold ørken og mister 20 guldmønter, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du farer vild i en kold ørken og mister 20 guldmønter, din balance er nu: ";
         return tekst;
     }
 
@@ -45,7 +45,7 @@ public class Felter {
     }
 
     public static String walled_city() {
-        String tekst = "Du finder 180 guldmønter uden for en befæstet by, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du finder 180 guldmønter uden for en befæstet by, din balance er nu: ";
         return tekst;
     }
 
@@ -55,7 +55,7 @@ public class Felter {
     }
 
     public static String monastery() {
-        String tekst = "Du går ind i et kloster, men finder ikke nogle guldmønter, din balance er stadig: " + Konto.getSaldo();
+        String tekst = "Du går ind i et kloster, men finder ikke nogle guldmønter, din balance er stadig: ";
         return tekst;
     }
 
@@ -65,7 +65,7 @@ public class Felter {
     }
 
     public static String black_cave() {
-        String tekst = "Du farer vild i en mørk grotte og mister 70 guldmønter, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du farer vild i en mørk grotte og mister 70 guldmønter, din balance er nu: ";
         return tekst;
     }
 
@@ -75,7 +75,7 @@ public class Felter {
     }
 
     public static String huts_in_the_mountain() {
-        String tekst = "Du finder nogle hytter i bjergene og finder 60 guldmønter i en af dem, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du finder nogle hytter i bjergene og finder 60 guldmønter i en af dem, din balance er nu: ";
         return tekst;
     }
 
@@ -85,7 +85,7 @@ public class Felter {
     }
 
     public static String the_werewall() {
-        String tekst = "På din rejse møder du en masse varulve, du skynder dig at løbe væk og taber 80 guldmønter ud af din lomme, men du får en ekstra tur, din balance er nu: " + Konto.getSaldo();
+        String tekst = "På din rejse møder du en masse varulve, du skynder dig at løbe væk og taber 80 guldmønter ud af din lomme, men du får en ekstra tur, din balance er nu: ";
         return tekst;
     }
 
@@ -95,7 +95,7 @@ public class Felter {
     }
 
     public static String the_pit() {
-        String tekst = "Du falder ned i et dybt hul og er nød til at betale en mand 50 guldmønter for at hjælpe dig op, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du falder ned i et dybt hul og er nød til at betale en mand 50 guldmønter for at hjælpe dig op, din balance er nu: ";
         return tekst;
     }
 
@@ -105,7 +105,7 @@ public class Felter {
     }
 
     public static String goldmine() {
-        String tekst = "Du går ind i en guldmine og finder 650 guldmønter, din balance er nu: " + Konto.getSaldo();
+        String tekst = "Du går ind i en guldmine og finder 650 guldmønter, din balance er nu: ";
         return tekst;
     }
 }
