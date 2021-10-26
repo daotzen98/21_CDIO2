ac22994eee6a873d74506fd7e85d439ccf471c2c
PeterM0ller <s215707@dtu.dk>
2021-10-26 13:30:52 +0200

    konto opdateret

--- src/Konto.java
+++ src/Konto.java
@@ -1,11 +1,19 @@
 public class Konto {
 
-    static int Saldo = 1000, x;
+    static int Saldo = 1000;
 
     public static int getSaldo(){
         return Saldo;
     }
     public static int addSaldo(int x){
         return Saldo += x;
+    }
+    static int Saldo1 = 1000;
+
+    public static int getSaldo1(){
+        return Saldo1;
+    }
+    public static int addSaldo1(int x){
+        return Saldo1 += x;
     }
 }
