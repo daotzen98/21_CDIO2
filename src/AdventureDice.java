f2a7953b0930b0569c1916c9a096f1a0785e7195
SimonP2602 <91070384+SimonP2602@users.noreply.github.com>
2021-10-26 14:53:06 +0200

    Fikset spiller klasse så der oprettes en ny "spiller-brik" for hver spiller inklusiv balance.

--- src/AdventureDice.java
+++ src/AdventureDice.java
@@ -5,12 +5,16 @@ public class AdventureDice {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Indtast navn: ");
-        String spiller1 = Spiller.getName();
+        String input1 = sc.next();
+        Spiller spiller1 = new Spiller(input1);
 
         System.out.println("Indtast navn: ");
-        String spiller2 = Spiller.getName();
+        String input2 = sc.next();
+        Spiller spiller2 = new Spiller(input2);
 
-
+        System.out.println("Spiller 1: " + spiller1 + "\nSpiller 2: " + spiller2);
+        spiller2.setSaldo(Felter.Tower());
+        System.out.println(Felter.tower());
         System.out.println("Spiller 1: " + spiller1 + "\nSpiller 2: " + spiller2);
 
     }
