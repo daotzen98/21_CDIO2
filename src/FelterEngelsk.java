d12026db587f41f19406a0e15104c76605806f8f
PeterM0ller <s215707@dtu.dk>
2021-10-26 11:21:12 +0200

    Har lavet en engelsk class

---
+++ src/FelterEngelsk.java
@@ -0,0 +1,111 @@
+public class FelterEngelsk {
+    public static int TowerEN() {
+        int x = 250;
+        return x;
+    }
+
+    public static String towerEN() {
+        String tekst = "Du går ind i et gammelt tårn og finder 250 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int CraterEN() {
+        int x = -100;
+        return x;
+    }
+
+    public static String craterEN() {
+        String tekst = "Du falder ned i et crater og mister 100 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int Palace_gatesEN() {
+        int x = 100;
+        return x;
+    }
+
+    public static String palace_gatesEN() {
+        String tekst = "Du finder 100 guldmønter udenfor portene til et palads, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int Cold_desertEN() {
+        int x = -20;
+        return x;
+    }
+
+    public static String cold_desertEN() {
+        String tekst = "Du farer vild i en kold ørken og mister 20 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int Walled_cityEN() {
+        int x = 180;
+        return x;
+    }
+
+    public static String walled_cityEN() {
+        String tekst = "Du finder 180 guldmønter uden for en befæstet by, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int MonasteryEN() {
+        int x = 0;
+        return x;
+    }
+
+    public static String monasteryEN() {
+        String tekst = "Du går ind i et kloster, men finder ikke nogle guldmønter, din balance er stadig: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int Black_caveEN() {
+        int x = -70;
+        return x;
+    }
+
+    public static String black_caveEN() {
+        String tekst = "Du farer vild i en mørk grotte og mister 70 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int Huts_in_the_mountainEN() {
+        int x = 60;
+        return x;
+    }
+
+    public static String huts_in_the_mountainEN() {
+        String tekst = "Du finder nogle hytter i bjergene og finder 60 guldmønter i en af dem, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int The_werewallEN() {
+        int x = -80;
+        return x;
+    }
+
+    public static String the_werewallEN() {
+        String tekst = "På din rejse møder du en masse varulve, du skynder dig at løbe væk og taber 80 guldmønter ud af din lomme, men du får en ekstra tur, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int The_pitEN() {
+        int x = -50;
+        return x;
+    }
+
+    public static String the_pitEN() {
+        String tekst = "Du falder ned i et dybt hul og er nød til at betale en mand 50 guldmønter for at hjælpe dig op, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+
+    public static int GoldmineEN() {
+        int x = 650;
+        return x;
+    }
+
+    public static String goldmineEN() {
+        String tekst = "Du går ind i en guldmine og finder 650 guldmønter, din balance er nu: " + Konto.getSaldo();
+        return tekst;
+    }
+}
