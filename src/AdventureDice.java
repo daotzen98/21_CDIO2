ce4738d06a4d76e58d63830f2cea37510df414ea
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-10-27 11:46:29 +0200

    Opdelt Felter, FelterDansk og FelterEngelsk. Gjort metoderne til one-liners

--- src/AdventureDice.java
+++ src/AdventureDice.java
@@ -16,47 +16,47 @@ public class AdventureDice {
 
         System.out.println("Du har slået to");
         Konto.addSaldo(Felter.Tower());
-        System.out.println(Felter.tower());
+        System.out.println(FelterDansk.tower());
 
         System.out.println("Du har slået tre");
         Konto.addSaldo(Felter.Crater());
-        System.out.println(Felter.crater());
+        System.out.println(FelterDansk.crater());
 
         System.out.println("Du har slået fire");
         Konto.addSaldo(Felter.Palace_gates());
-        System.out.println(Felter.palace_gates());
+        System.out.println(FelterDansk.palace_gates());
 
         System.out.println("Du har slået fem");
         Konto.addSaldo(Felter.Cold_desert());
-        System.out.println(Felter.cold_desert());
+        System.out.println(FelterDansk.cold_desert());
 
         System.out.println("Du har slået seks");
         Konto.addSaldo(Felter.Walled_city());
-        System.out.println(Felter.walled_city());
+        System.out.println(FelterDansk.walled_city());
 
         System.out.println("Du har slået syv");
         Konto.addSaldo(Felter.Monastery());
-        System.out.println(Felter.monastery());
+        System.out.println(FelterDansk.monastery());
 
         System.out.println("Du har slået otte");
         Konto.addSaldo(Felter.Black_cave());
-        System.out.println(Felter.black_cave());
+        System.out.println(FelterDansk.black_cave());
 
         System.out.println("Du har slået ni");
         Konto.addSaldo(Felter.Huts_in_the_mountain());
-        System.out.println(Felter.huts_in_the_mountain());
+        System.out.println(FelterDansk.huts_in_the_mountain());
 
         System.out.println("Du har slået ti");
         Konto.addSaldo(Felter.The_werewall());
-        System.out.println(Felter.the_werewall());
+        System.out.println(FelterDansk.the_werewall());
 
         System.out.println("Du har slået elleve");
         Konto.addSaldo(Felter.The_pit());
-        System.out.println(Felter.the_pit());
+        System.out.println(FelterDansk.the_pit());
 
         System.out.println("Du har slået tolv");
         Konto.addSaldo(Felter.Goldmine());
-        System.out.println(Felter.goldmine());
+        System.out.println(FelterDansk.goldmine());
 
 
 
--- src/FelterEngelsk.java
+++ src/FelterEngelsk.java
@@ -1,111 +1,46 @@
 public class FelterEngelsk {
-    public static int TowerEN() {
-        int x = 250;
-        return x;
-    }
 
     public static String towerEN() {
-        String tekst = "You enter a dark old tower and find 250 gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int CraterEN() {
-        int x = -100;
-        return x;
+        return "You enter a dark old tower and find 250 gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String craterEN() {
-        String tekst = "You fall down in a crater and drop 100 gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int Palace_gatesEN() {
-        int x = 100;
-        return x;
+        return "You fall down in a crater and drop 100 gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String palace_gatesEN() {
-        String tekst = "You find 100 gold coins at the gates of the palace. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int Cold_desertEN() {
-        int x = -20;
-        return x;
+        return "You find 100 gold coins at the gates of the palace. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String cold_desertEN() {
-        String tekst = "You get lost in the cold desert and lose 20 gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int Walled_cityEN() {
-        int x = 180;
-        return x;
+        return "You get lost in the cold desert and lose 20 gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String walled_cityEN() {
-        String tekst = "You find 180 gold coins outside the walled city. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int MonasteryEN() {
-        int x = 0;
-        return x;
+        return "You find 180 gold coins outside the walled city. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String monasteryEN() {
-        String tekst = "You enter a monastery but you do not find any gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int Black_caveEN() {
-        int x = -70;
-        return x;
+        return "You enter a monastery but you do not find any gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String black_caveEN() {
-        String tekst = "You get lost in the black cave and misplace 70 gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int Huts_in_the_mountainEN() {
-        int x = 60;
-        return x;
+        return "You get lost in the black cave and misplace 70 gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String huts_in_the_mountainEN() {
-        String tekst = "You find some huts in the mountain and acquire 60 gold coins. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int The_werewallEN() {
-        int x = -80;
-        return x;
+        return "You find some huts in the mountain and acquire 60 gold coins. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String the_werewallEN() {
-        String tekst = "You encounter a werewall and rapidly try to run away. Sadly, you lose 80 gold coins during your runaway but you receive another turn. Your balance remains unchanged: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int The_pitEN() {
-        int x = -50;
-        return x;
+        return "You encounter a werewall and rapidly try to run away. Sadly, you lose 80 gold coins during your runaway but you receive another turn. Your balance remains unchanged: " + Konto.getSaldo();
     }
 
     public static String the_pitEN() {
-        String tekst = "You fall into a pit and you pay 50 gold coins to get help. Your new balance is: " + Konto.getSaldo();
-        return tekst;
-    }
-
-    public static int GoldmineEN() {
-        int x = 650;
-        return x;
+        return "You fall into a pit and you pay 50 gold coins to get help. Your new balance is: " + Konto.getSaldo();
     }
 
     public static String goldmineEN() {
-        String tekst = "You walk into the gold mine and gets 650 gold coins. Your new balance is now: " + Konto.getSaldo();
-        return tekst;
+        return "You walk into the gold mine and gets 650 gold coins. Your new balance is now: " + Konto.getSaldo();
     }
 }
