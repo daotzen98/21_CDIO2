59bca88d14020dec28054191fc1aaaf0a6bc8fa5
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-10-27 12:29:36 +0200

    Fjernet Konto.getSaldo fra Felter

--- src/FelterEngelsk.java
+++ src/FelterEngelsk.java
@@ -1,46 +1,46 @@
 public class FelterEngelsk {
 
     public static String towerEN() {
-        return "You enter a dark old tower and find 250 gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You enter a dark old tower and find 250 gold coins. Your new balance is: ";
     }
 
     public static String craterEN() {
-        return "You fall down in a crater and drop 100 gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You fall down in a crater and drop 100 gold coins. Your new balance is: ";
     }
 
     public static String palace_gatesEN() {
-        return "You find 100 gold coins at the gates of the palace. Your new balance is: " + Konto.getSaldo();
+        return "You find 100 gold coins at the gates of the palace. Your new balance is: ";
     }
 
     public static String cold_desertEN() {
-        return "You get lost in the cold desert and lose 20 gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You get lost in the cold desert and lose 20 gold coins. Your new balance is: ";
     }
 
     public static String walled_cityEN() {
-        return "You find 180 gold coins outside the walled city. Your new balance is: " + Konto.getSaldo();
+        return "You find 180 gold coins outside the walled city. Your new balance is: ";
     }
 
     public static String monasteryEN() {
-        return "You enter a monastery but you do not find any gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You enter a monastery but you do not find any gold coins. Your new balance is: ";
     }
 
     public static String black_caveEN() {
-        return "You get lost in the black cave and misplace 70 gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You get lost in the black cave and misplace 70 gold coins. Your new balance is: ";
     }
 
     public static String huts_in_the_mountainEN() {
-        return "You find some huts in the mountain and acquire 60 gold coins. Your new balance is: " + Konto.getSaldo();
+        return "You find some huts in the mountain and acquire 60 gold coins. Your new balance is: ";
     }
 
     public static String the_werewallEN() {
-        return "You encounter a werewall and rapidly try to run away. Sadly, you lose 80 gold coins during your runaway but you receive another turn. Your balance remains unchanged: " + Konto.getSaldo();
+        return "You encounter a werewall and rapidly try to run away. Sadly, you lose 80 gold coins during your runaway but you receive another turn. Your balance remains unchanged: ";
     }
 
     public static String the_pitEN() {
-        return "You fall into a pit and you pay 50 gold coins to get help. Your new balance is: " + Konto.getSaldo();
+        return "You fall into a pit and you pay 50 gold coins to get help. Your new balance is: ";
     }
 
     public static String goldmineEN() {
-        return "You walk into the gold mine and gets 650 gold coins. Your new balance is now: " + Konto.getSaldo();
+        return "You walk into the gold mine and gets 650 gold coins. Your new balance is now: ";
     }
 }
